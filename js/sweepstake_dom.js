5f0850dd64241a708a0bd69588adcf9bb02601d6
moyden <joemoyden@gmail.com>
2018-06-25 14:24:03 +0100

    some changes

--- js/sweepstake_dom.js
+++ js/sweepstake_dom.js
@@ -11,9 +11,32 @@ const vm = new Vue({
 
   computed: {
     sorted: function () {
-      return this.teams.sort(function (a, b) {
-        if (a.gc !== b.gc) return a.gc < b.gc ? 1 : -1;else return a.name > b.name ? 1 : -1;
+      // return this.teams.sort(function (a, b) {
+      //   if (a.gc !== b.gc) return a.gc < b.gc ? 1 : -1
+      //   else return a.name > b.name ? 1 : -1
+      // })
+      return this.teams;
+    },
+
+    aggregate: function () {
+      let output = [];
+      this.teams.forEach(team => {
+        let found = false;
+        output.forEach(d => {
+          if (d.value === team.gc) {
+            found = true;
+            d.teams.push(team);
+          }
+        });
+        if (!found) {
+          output.push({
+            value: team.gc,
+            teams: [team]
+          });
+        }
       });
+      output.sort((a, b) => b.value - a.value);
+      return output;
     }
   },
 
@@ -34,7 +57,7 @@ const vm = new Vue({
 
     fetchData: function () {
       let self = this;
-      d3.json('http://api.football-data.org/v1/competitions/467/fixtures', {
+      d3.json('https://api.football-data.org/v1/competitions/467/fixtures', {
         headers: { 'X-Auth-Token': 'ad8f2917e7174b859a89e00b4127b407' }
       }).then(function (data) {
         const fixtures = data.fixtures.map(d => {
